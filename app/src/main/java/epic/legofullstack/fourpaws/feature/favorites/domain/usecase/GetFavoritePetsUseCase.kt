18330a1ff629296be335e588af5ed7aa4a24d255
AsanovAlbek <87094098+AsanovAlbek@users.noreply.github.com>
2022-10-21 18:12:08 +0300

    Добавил в фичу favorites заготовки для MVI

--- app/src/main/java/epic/legofullstack/fourpaws/feature/favorites/domain/usecase/GetFavoritePetsUseCase.kt
+++ app/src/main/java/epic/legofullstack/fourpaws/feature/favorites/domain/usecase/GetFavoritePetsUseCase.kt
@@ -5,6 +5,7 @@ import epic.legofullstack.fourpaws.feature.favorites.data.mapper.toFavoritePet
 import epic.legofullstack.fourpaws.feature.favorites.di.FavoritesModule
 import epic.legofullstack.fourpaws.feature.favorites.domain.model.FavoritePet
 import epic.legofullstack.fourpaws.feature.favorites.domain.repository.FavoritesRepository
+import epic.legofullstack.fourpaws.network.errorhandle.safeCall
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
@@ -14,7 +15,9 @@ class GetFavoritePetsUseCase(
     @DispatchersModule.IoDispatcher
     private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO
 ) {
-    suspend operator fun invoke() : List<FavoritePet> = withContext(ioDispatcher) {
-        return@withContext repository.getFavorites().map { it.toFavoritePet() }.filter { it.isFavorite }
+    suspend operator fun invoke() = withContext(ioDispatcher) {
+        return@withContext safeCall {
+            repository.getFavorites().map { it.toFavoritePet() }.filter { it.isFavorite }
+        }
     }
 }
