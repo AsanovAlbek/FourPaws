010005c83bcced8381d007c140edb1e8d2c76900
AlexaPan <panina.Alexa5@gmail.com>
2022-11-09 23:53:40 +0300

    получение данных из firebase для списков областей и списка питомцев

--- app/src/main/java/epic/legofullstack/fourpaws/feature/location/data/AreaRepositoryImpl.kt
+++ app/src/main/java/epic/legofullstack/fourpaws/feature/location/data/AreaRepositoryImpl.kt
@@ -2,17 +2,14 @@ package epic.legofullstack.fourpaws.feature.location.data
 
 import epic.legofullstack.fourpaws.core.data.model.AreaDto
 import epic.legofullstack.fourpaws.feature.location.domain.repository.AreaRepository
+import epic.legofullstack.fourpaws.network.firebase.data.FirebaseDataSource
 
-//TODO получать данные извне
-class AreaRepositoryImpl : AreaRepository {
-    override suspend fun getAreas(): List<AreaDto> {
-        return listOf(
-            AreaDto(1, "Komi Republic"),
-            AreaDto(2, "Krasnodar Territory"),
-            AreaDto(3, "Amur Region"),
-            AreaDto(4, "Moscow Region"),
-            AreaDto(5, "Santa Clara County")
-        )
+class AreaRepositoryImpl(
+    private val dataSource: FirebaseDataSource
+) : AreaRepository {
+    override suspend fun getAreas(): List<AreaDto> = dataSource.getObjects(AREA_COLLECTION, AreaDto::class.java)
+
+    companion object {
+        private const val AREA_COLLECTION = "area"
     }
-
 }
