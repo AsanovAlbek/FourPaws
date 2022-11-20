4d72a76cebecbec8de2d07266254de97f503a93b
AlexaPan <panina.Alexa5@gmail.com>
2022-11-20 18:51:18 +0300

    заглушка для фильтра

--- app/src/main/java/epic/legofullstack/fourpaws/feature/detail/presentation/adapter/ImagesSlider.kt
+++ app/src/main/java/epic/legofullstack/fourpaws/feature/detail/presentation/adapter/ImagesSlider.kt
@@ -5,10 +5,8 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
-import com.bumptech.glide.request.RequestOptions
 import epic.legofullstack.fourpaws.R
 import epic.legofullstack.fourpaws.databinding.SliderItemBinding
-import jp.wasabeef.glide.transformations.BlurTransformation
 
 class ImagesSlider(
     private val images: List<String>
